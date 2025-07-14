80a48ab96f4e72ee0d3dea70713c4aa7c125e8f0
Kaiy9 <dejiuzhidao@gmail.com>
2025-07-14 23:53:00 +0800

    feat: implement custom homepage component

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,11 +1,16 @@
+// 正确的 docs/.vitepress/theme/index.js 文件内容
+
 import DefaultTheme from 'vitepress/theme'
-import CustomHome from './components/CustomHome.vue'
-import './custom.css'
+import CustomHome from './CustomHome.vue' // 修正了这里的路径
+import './custom.css' // 如果您有自定义CSS，可以保留这行
+// import './style.css' // 如果您有自定义CSS，可以保留这行
 
 export default {
-  extends: DefaultTheme,
+  // 修正了这里的语法，使用展开语法来继承默认主题
+  ...DefaultTheme, 
+  
   enhanceApp({ app }) {
+    // 在这里注册您的自定义组件
     app.component('CustomHome', CustomHome)
   }
 }
-
